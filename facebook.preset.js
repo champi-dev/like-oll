dc36de1934217a84db3de23cd6a63d79273e124e
Champi <danielsarcor@gmail.com>
2021-02-28 23:40:19 -0500

    Update facebook.preset.js

--- facebook.preset.js
+++ facebook.preset.js
@@ -28,3 +28,44 @@ function clickManyEls () {
 
   window.scrollTo(0, window.scrollY + scrollBy)
 }
+
+/*
+  Work in progress variant:
+
+ const delay = 10000
+const target = "[aria-label='Me gusta']"
+const scrollBy = 1000
+
+clickManyEls()
+setInterval(clickManyEls, delay)
+
+function clickEl({ selector, element }) {
+  const el = element || document.querySelector(selector)
+  const { x, y } = el.getBoundingClientRect()
+
+  el.dispatchEvent(
+    new MouseEvent('click', {
+      clientX: x,
+      clientY: y,
+      isTrusted: true,
+      bubbles: true
+    }))
+ }
+
+function clickManyEls () {
+  const likeBtns = document.querySelectorAll(target)
+
+
+    likeBtns.forEach((btn, i) => {
+    if (i + 1 === 10) {return}
+
+        setTimeout(() => {
+            clickEl({
+            element: likeBtns[i]
+            })
+        }, (delay / 10) * (i + 1))
+    })
+
+  window.scrollTo(0, window.scrollY + scrollBy)
+} 
+*/
