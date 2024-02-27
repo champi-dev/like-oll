8e2daaa3d58569ffc8b957087b1b2ff4af0f9463
Daniel <danielsarcor@gmail.com>
2024-02-27 14:26:18 -0500

    Update tiktok.like.preset.js

--- tiktok.like.preset.js
+++ tiktok.like.preset.js
@@ -1,21 +1,17 @@
-/* Work in progress */
+(() => {
+/* Tiktok like preset */
 
-const waitFor = 10000
-const scrollBy = 1000
+const delay = 300000 // 5min
+const likeBy = 30
+const target = '[data-e2e="like-icon"]'
+const scrollBy = 15000 * likeBy
 
-const hiddenBtns = document.querySelectorAll('[data-e2e="like-icon"]')
-clickLikeBtn(hiddenBtns[0].parentElement)
-
-setInterval(() => {
-    const hiddenBtns = document.querySelectorAll('[title="like"]')
-    hiddenBtns.forEach(btn => clickEl({element: btn.parentElement}))
-    window.scrollTo(0, window.scrollY + (scrollBy * hiddenBtns.length))
-}, waitFor)
+clickManyEls()
+setInterval(clickManyEls, delay)
 
 function clickEl({ selector, element }) {
   const el = element || document.querySelector(selector)
   const { x, y } = el.getBoundingClientRect()
-
   el.dispatchEvent(
     new MouseEvent('click', {
       clientX: x,
@@ -23,12 +19,19 @@ function clickEl({ selector, element }) {
       isTrusted: true,
       bubbles: true
     }))
-}
+ }
 
-function clickLikeBtn (btn) {
-    clickEl({
-        element: btn
+function clickManyEls () {
+  const likeBtns = document.querySelectorAll(target)
+    likeBtns.forEach((btn, i) => {
+    if (i + 1 === 10) {return} // Only like the first 10
+        setTimeout(() => {
+            clickEl({
+            element: likeBtns[i]
+            })
+            console.log('like')
+        }, 1000 * (i + 1))
     })
-    console.log('like')
-    
-}
+  window.scrollTo(0, window.scrollY + scrollBy)
+} 
+})()
